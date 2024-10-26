e8378e293555f9b7a1e2088b50918c571e2a42c7
Muneeb1912 <muneeburrehman1912@gmail.com>
2024-10-26 15:30:24 +0500

    changes3

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Flex, Link, Button, useBoolean } from "@chakra-ui/react";
+import { Flex, Link, Button, useBoolean} from "@chakra-ui/react";
 import { FaBars } from "react-icons/fa6";
 import { HashLink } from "react-router-hash-link";
 const Navbar = () => {
@@ -37,7 +37,6 @@ const Navbar = () => {
           gap={"20px"}
           w={{ base: "full", md: "fit-content" }}
           flexDirection={{ base: "column", md: "row" }}
-          transition={'all 1s'}
           h={{base:active?'fit-content':'0', md:'fit-content'}}
         >
           {navitems.map((item, index) => (
@@ -46,11 +45,6 @@ const Navbar = () => {
               key={index}
               fontSize={16}
               w={{base:'full',md:'fit-content'}}
-              pos={'relative'}
-              _after={{content:'" "',w:"0",h:"4px",bg:"white",display:'flex',pos:'absolute',bottom:'0' ,transition:'.3s'}}
-              _hover={{
-                _after:{w:"100%"}
-                }}
             className="navBtn"
             to={item.path}
             >
